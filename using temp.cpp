16a77c95502fee83d2066f00d47e00c6d96c418a
RAVI MAKVANA <aaa888aaa99910@gmail.com>
2024-07-22 10:52:14 +0530

    Add files via upload

---
+++ using temp.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+            int a=5;
+            int b=10;
+            int c=15;
+            int temp;
+            
+            temp=a;
+            a=b;
+            b=c;
+            c=temp;
+
+            cout<< a<<endl ;
+            cout<< b<<endl ;
+            cout<< c<<endl ;
+            
+            
+            return 0;
+}            
+    
